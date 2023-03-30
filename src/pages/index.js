741aff356f474d01a333a217aea110649fd980ef
Ramond Falcão <ramondfalcao@gmail.com>
2023-03-30 10:38:08 -0300

    Assets Next.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import styles from '../styles/Home.module.css'
 import Head from 'next/head'
+import Image from 'next/image'
+import cityImage from '../../public/images/city.jpeg'
 
 export default function Home() {
   return (
@@ -14,6 +16,16 @@ export default function Home() {
       </Head>
       <div className={styles.container}>
         <h1>Hello World! Nexts.js</h1>
+        <Image
+          alt="City"
+          src={cityImage}
+          width={1000}
+          height={1000}
+          style={{
+            maxWidth: '100%',
+            height: 'auto',
+          }}
+        />
       </div>
     </>
   )
